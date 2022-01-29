0406e9a039fcdea96173fbb72d0f050636aa871c
frozenfire1984 <frozen-fire@mail.ru>
2022-01-29 03:34:02 +0300

    created base app

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {createStore} from 'redux'
 import './App.css';
 
-function App() {
+const initialState = {
+  name: "Will",
+  secondname: "Smith"
+}
+
+const reducer = (state = initialState,action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {...state, name: action.payload}
+    case 'CHANGE_SECONDNAME':
+      return {...state, secondname: action.payload}
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState())
+
+const changeName = {
+  type: 'CHANGE_NAME',
+  payload: 'Bill'
+}
+
+const changeSecondName = {
+  type: 'CHANGE_SECONDNAME',
+  payload: 'Goldberg'
+}
+
+store.dispatch(changeName);
+
+console.log(store.getState())
+
+store.dispatch(changeSecondName);
+
+console.log(store.getState())
+
+//lesson: https://www.youtube.com/watch?v=jvkhKRHS2TY&t=4s
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      redux
     </div>
   );
 }
